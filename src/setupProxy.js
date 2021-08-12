536f987cc74f73548b8e7a1c5d09c45a3fdf715b
segunsub <segunsubair3@gmail.com>
2021-08-12 17:08:48 -0400

    changes

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const  createProxyMiddleware = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    console.log(app)
+  app.use(
+    '/socket.io',
+    createProxyMiddleware({
+      target: 'https://shared-piano-backend.herokuapp.com',
+      changeOrigin: true,
+    })
+  );
+};
