c20b27fbf19baed471965fba526e94f036ed1dc6
segunsub <segunsubair3@gmail.com>
2021-08-12 13:14:12 -0400

    update

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ ReactDOM.render(
   <React.StrictMode>
     <SharedPiano>
       <KeyNote>
-    <Router>
-      <App />
-    </Router>
+      <Router>
+       <App />
+     </Router>
     </KeyNote>
     </SharedPiano>
   </React.StrictMode>,
