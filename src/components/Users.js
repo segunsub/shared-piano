d07102796a8052719974300c0f9e25e7151dcf9a
segunsub <segunsubair3@gmail.com>
2021-08-16 12:27:49 -0400

    changes

--- src/components/Users.js
+++ src/components/Users.js
@@ -15,13 +15,7 @@ function Users({ color }) {
             <ul id='users_list'>
                 {users.map((users, i) => {
                     if(users.color === color) {
-<<<<<<< HEAD
                          return <li style={{color: users.color}} className='users' key={users.id}><span key={i} style={{color: 'black'}}>Current: </span>{users.name}</li> 
-=======
-                         return <li style={{color: users.color}} className='users' key={users.id}><span key={i} style={{color: 'black'}}>User: </span>{users.name}</li> 
-                                
-                
->>>>>>> 3e5f8ae (finishing touches)
                     }else {
                         return <li style={{color: users.color}} className='users' key={users.id}>{users.name}</li>
                     }
